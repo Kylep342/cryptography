85c00431d1595fcbdc63cd9c50b87db02d6f0f2a
Kylep342 <kylep342@gmail.com>
2018-05-26 23:09:57 -0500

    now processes mulitple words (i.e. a true message)

--- caesarian.py
+++ caesarian.py
@@ -1,18 +1,24 @@
+#!/usr/bin/env python
 import sys
 
-def caesarian(string, shift_by):
-    shifted = []
-    for char in string:
-        if char.isalpha():
-            if char.isupper():
-                shifted.append(chr((ord(char) - ord('A') + shift_by) % 26 + ord('A')))
-            else:
-                shifted.append(chr((ord(char) - ord('a') + shift_by) % 26 + ord('a')))
+
+def caesarian(shift_by, word_list):
+    shifted_args = []
+    for string in word_list:
+        shifted = []
+        for char in string:
+            if char.isalpha():
+                if char.isupper():
+                    shifted.append(chr((ord(char) - ord('A') + shift_by) % 26 + ord('A')))
+                else:
+                    shifted.append(chr((ord(char) - ord('a') + shift_by) % 26 + ord('a')))
         else:
             shifted.append(char)
-    return ''.join(shifted)
+        shifted_word = ''.join(shifted)
+        shifted_args.append(shifted_word)
+    return shifted_args
 
 
 if __name__ == '__main__':
     for shift in range(26):
-        print(str(shift) + ': ' + caesarian(sys.argv[1], shift))
+        print(str(shift) + ': ' + ' '.join(caesarian(shift, sys.argv[1:])))
